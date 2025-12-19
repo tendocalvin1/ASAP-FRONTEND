17d83a15209f4267ade03a06dc857848c8342fa9
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2025-12-19 11:05:55 +0300

    I have designed a fully functioning expense tracker using HTML,CSS and JavaScript

--- Expense-Tracker/script.js
+++ Expense-Tracker/script.js
@@ -8,49 +8,92 @@ const amountEl = document.getElementById("amount");
 
 let transactions = JSON.parse(localStorage.getItem("transactions")) || [];
 
-transactionFormEl.addEventListener("submit", addTransaction)
+transactionFormEl.addEventListener("submit", addTransaction);
 
-function addTransaction(e){
-    e.preventDefault(); // this will prevent the form from refreshing itself
+function addTransaction(e) {
+  e.preventDefault();
 
-    // getting form values
-    const description = descriptionEl.value.trim();
-    const amount = parseFloat(amountEl.value);
+  // get form values
+  const description = descriptionEl.value.trim();
+  const amount = parseFloat(amountEl.value);
 
-    transactions.push({
-        id: Date.now(),
-        description,
-        amount
-    })
+  transactions.push({
+    id: Date.now(),
+    description,
+    amount,
+  });
 
-    localStorage.setItem("transactions", JSON.stringify(transactions));
+  localStorage.setItem("transactions", JSON.stringify(transactions));
 
-    updateTransactionList();
-    updateSummary();
+  updateTransactionList();
+  updateSummary();
 
-
-    transactionFormEl.reset();
+  transactionFormEl.reset();
 }
 
-function updateTransactionList(){
-    transactionListEl.innerHTML = "";
+function updateTransactionList() {
+  transactionListEl.innerHTML = "";
 
-    const sortedTransactions = [...transactions].reverse();
+  const sortedTransactions = [...transactions].reverse();
 
-    sortedTransactions.forEach((transaction)=>{
-        const transactionEl = createTransactionElement(transaction)
-        transactionListEl.appendChild(transactionEl)
-    })
+  sortedTransactions.forEach((transaction) => {
+    const transactionEl = createTransactionElement(transaction);
+    transactionListEl.appendChild(transactionEl);
+  });
 }
 
-function createTransactionElement(transaction){
-    const li = document.createElement("li");
-    li.classList.add("transaction");
-    li.classList.add(transaction.amount > 0 ? "income": "expense");
+function createTransactionElement(transaction) {
+  const li = document.createElement("li");
+  li.classList.add("transaction");
+  li.classList.add(transaction.amount > 0 ? "income" : "expense");
 
-    li.innerHTML = `
+  li.innerHTML = `
     <span>${transaction.description}</span>
-    <span>${transaction.amount}</span>
+    <span>
+  
+    ${formatCurrency(transaction.amount)}
+      <button class="delete-btn" onclick="removeTransaction(${transaction.id})">x</button>
+    </span>
+  `;
 
-    `
+  return li;
 }
+
+function updateSummary() {
+  // 100, -50, 200, -200 => 50
+  const balance = transactions.reduce((acc, transaction) => acc + transaction.amount, 0);
+
+  const income = transactions
+    .filter((transaction) => transaction.amount > 0)
+    .reduce((acc, transaction) => acc + transaction.amount, 0);
+
+  const expenses = transactions
+    .filter((transaction) => transaction.amount < 0)
+    .reduce((acc, transaction) => acc + transaction.amount, 0);
+
+  // update ui => todo: fix the formatting
+  balanceEl.textContent = formatCurrency(balance);
+  incomeAmountEl.textContent = formatCurrency(income);
+  expenseAmountEl.textContent = formatCurrency(expenses);
+}
+
+function formatCurrency(number) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(number);
+}
+
+function removeTransaction(id) {
+  // filter out the one we wanted to delete
+  transactions = transactions.filter((transaction) => transaction.id !== id);
+
+  localStorage.setItem("transcations", JSON.stringify(transactions));
+
+  updateTransactionList();
+  updateSummary();
+}
+
+// initial render
+updateTransactionList();
+updateSummary();
