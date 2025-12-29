5345feb16243b356033804ed94d25b47cc46d5e7
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2025-12-29 15:30:43 +0300

    I have remained myself about the basics of Vanilla JavaScript like operators and variables

--- Expense-Tracker/Todo-App/Business-Website/operators.js
+++ Expense-Tracker/Todo-App/Business-Website/operators.js
@@ -13,9 +13,33 @@ console.log(a+b*b);
 
 let foxes = 18
 foxes +=1;
-foxes -=1;
-foxes /=2;
-foxes *=2;
-foxes **=2;
-foxes %=7;
 console.log(foxes)
+foxes -=1;
+console.log(foxes)
+foxes /=2;
+console.log(foxes)
+foxes *=2;
+console.log(foxes)
+foxes **=2;
+console.log(foxes)
+foxes %=6;
+console.log(foxes)
+
+let goals = 5;
+goals++;
+console.log(goals);
+
+goals--;
+console.log(goals);
+// comparison operators
+
+let m = 5;
+let n = 10;
+console.log(n < m);
+console.log(n > m);
+console.log(n <= m);
+console.log(n >= m);
+console.log(n == m);
+console.log(n != m);  
+console.log(5 == 5); // true
+console.log(5 === "5"); // false   
