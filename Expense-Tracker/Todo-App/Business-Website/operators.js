5cd3ad5086b780d5a6980b0689413aa9ffa4d1b6
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2025-12-29 15:18:50 +0300

    The goal is to get a role as a Full stack developer by July 2026, by God's grace

---
+++ Expense-Tracker/Todo-App/Business-Website/operators.js
@@ -0,0 +1,21 @@
+// operators in JavaScript
+// Arithmetic operators
+let a = 10;
+let b = 5;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** b);
+
+console.log(a+b*b);
+
+let foxes = 18
+foxes +=1;
+foxes -=1;
+foxes /=2;
+foxes *=2;
+foxes **=2;
+foxes %=7;
+console.log(foxes)
---
+++ Expense-Tracker/Todo-App/Business-Website/variables.js
@@ -0,0 +1,35 @@
+let username = "Tendo Calvin";
+console.log(`My name is ${username}`);
+
+username = "Expressions Oozing";
+console.log(`My name is ${username}`);
+
+username = "Robbie Lyle";
+console.log(`My name is ${username}`);
+
+// const variables
+const siteName = "Expense Tracker Todo App";
+console.log(`Welcome to ${siteName}`);
+
+// data types in JavaScript.
+// Primitives: string, number, boolean, null, undefined, symbol, bigint
+// Non-primitives: object, array, function
+
+let age = 25; // number
+let name = "Allan"; // string
+
+let isStudent = true; // boolean
+console.log(`${name} is a student: ${isStudent}`);
+console.log(7 + 4)
+
+let score = 4;
+score +=1 
+console.log(`Updated score: ${score}`);
+console.log(score <= 6)
+
+let height = 5;
+if(height > 8){
+    console.log("The man is very tall")
+}else{
+    console.log("He's very short")
+}
