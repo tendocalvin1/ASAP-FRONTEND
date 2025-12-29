c4de3e51132acbf60ecf4a3907c1eafc2fab4f96
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2025-12-29 16:49:20 +0300

    I am learning DOM manipulation in Vanilla Javascript so that I can become the very best Software Engineer by June 2026 so that I am able to land a remote role, inshAllah

---
+++ Expense-Tracker/Todo-App/Business-Website/functions.js
@@ -0,0 +1,29 @@
+// Functions in JavaScript
+// A function is a reusable block of code. 
+
+function sayHello(name){
+    console.log(`Hello, ${name}. I want to be employed as a full stack developer by June 2026.`)
+}
+
+sayHello("Tendo");
+
+function addNumbers(num1, num2){
+    console.log(`The sum of these numbers is : ${num1 + num2}`);
+}
+
+function multiplyNumbers(num1, num2){
+    console.log(`The product of these numbers is : ${num1 * num2}`);
+}
+
+function subtractNumbers(num1, num2){
+    console.log(`The difference of these numbers is : ${num1 - num2}`);
+}
+
+function divisionNumbers(num1, num2){
+    console.log(`The result of these numbers is : ${num1 / num2}`);
+}
+
+addNumbers(2,4)
+multiplyNumbers(2,4)
+subtractNumbers(2,4)
+divisionNumbers(2,4)
