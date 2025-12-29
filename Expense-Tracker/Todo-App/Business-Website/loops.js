2ced6607438b6210594f190ee6cd2210e1309e2b
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2025-12-29 16:12:47 +0300

    Basics of any programming language as I try to learn Full stack development so that I can get a job by June, 2026, by God's grace

---
+++ Expense-Tracker/Todo-App/Business-Website/loops.js
@@ -0,0 +1,19 @@
+// Loops in JavaScript
+// while loop
+
+let counter = 1;
+while (counter < 10){
+    counter++;
+    console.log(`Counter is at: ${counter}`);
+}
+
+let i = 0;
+while (i < 5){
+    i++;
+    console.log(`The number is now :${i}`);
+}
+
+// for loop
+for (let m = 7; m <= 10; m++){
+    console.log(`The number is now: ${m}`);
+}
